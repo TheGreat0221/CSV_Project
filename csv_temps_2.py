2416330040a10af61864dbef5c7d3e000efe2de9
Green <Alex_Green2@baylor.edu>
2020-01-27 15:46:12 -0600

    csv_temps 1-3 for project

--- csv_temps_2.py
+++ csv_temps_2.py
@@ -13,16 +13,20 @@ header_row = next(csv_file)
 ##    print(index,column_header)
 
 highs = []
+dates = []
 
 for row in csv_file:
     highs.append(int(row[5]))
+    dates.append(row[2])
 
-print(highs[:10])
+fig = plt.figure()
 
-plt.plot(highs,color='red')
+plt.plot(dates,highs,color='red')
 plt.title("Daily High Temps, July 2018",fontsize=16)
-plt.xlabel("",fontsize=16)
-plt.ylabel("Temperature (F)",fontsize=16)
-plt.tick_params(axis='both',which="major",labelsize=16)
+plt.xlabel("",fontsize=12)
+plt.ylabel("Temperature (F)",fontsize=12)
+plt.tick_params(axis='both',which="major",labelsize=12)
+
+fig.autofmt_xdate() ## what is on the x axis is a date, so format correctly; draws the date labels diagonally to prevent them from overlapping
 
 plt.show()
